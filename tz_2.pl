c45c33ccbfae4194af79c6230b05c77a316abbfd
AlenaGal <79111863+AlenaGal@users.noreply.github.com>
2022-11-07 02:00:33 +0300

    Add files via upload

---
+++ tz_2.pl
@@ -0,0 +1,36 @@
+package participant_of_the_battle; 	# создание класса "Участник сражения”
+sub new {
+	my $class = shift;
+   	my $self = {
+		_health => shift;
+   	};
+   	bless $self, $class;
+   	return $self;
+}
+
+sub to_die { 				# создание метода “погибнуть”
+	
+	}
+
+
+package kotsi;
+sub spear_damage {
+		
+	}
+sub saddle_the_horse {
+	
+	}
+sub heal {
+	
+	}
+	
+
+
+package giant;
+sub clud_damage {
+	
+	}
+sub bewitch {
+	
+	}
+
